f0a8ed293e3586bb1ba185f6eee4942501a3ea82
Michael Anckaert <michael.anckaert@sinax.be>
2021-10-05 19:31:26 +0200

    Get message from greeter-service

--- app.go
+++ app.go
@@ -1,19 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 )
 
+type Message struct {
+	Message string `json:"message"`
+}
+
+func getMessage() (*Message, error) {
+	greeterServiceHostname := "greeter-service"
+	url := fmt.Sprintf("http://%s/", greeterServiceHostname)
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var message Message
+	json.Unmarshal(responseData, &message)
+	return &message, nil
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	message := os.Getenv("MESSAGE")
-	if len(message) == 0 {
-		message = "Hello"
+	message, err := getMessage()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	environment := os.Getenv("ENVIRONMENT")
-	fmt.Fprintf(w, "%s world! We're running in %s", message, environment)
+	fmt.Fprintf(w, "%s We're running in environment %s", message.Message, environment)
 }
 
 func main() {
